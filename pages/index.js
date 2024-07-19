748efbd88098996af7a8fb05b8d1e242817750cf
Ashley Hart <ashleyhart@Ashleys-MacBook-Pro.local>
2024-07-19 16:02:26 -0600

    editing stage

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Trade Goods, Get Online</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
