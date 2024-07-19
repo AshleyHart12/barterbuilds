aca4ede057349f87e75e27dbe02432ac3fa5beae
Ashley Hart <ashleyhart@Ashleys-MacBook-Pro.local>
2024-07-19 15:12:25 -0600

    testing changes

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <Header title="Barter Builds" />
+        <p className="description">Trade Goods, Get Online
         </p>
       </main>
 
